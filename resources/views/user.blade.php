cba0ffd33792a6bd2f1d7acc9f4659ea67170daa
Sean Montgomery <seancmont@gmail.com>
2017-08-24 12:29:58 -0400

    adding feature to upload profile picture

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -21,6 +21,12 @@
 			    <td>Beast</td>
 			  </tr>
 			</table>
+
+			<form enctype="multipart/form-data" action="uploader.php" method="POST">
+			<input type="hidden" name="MAX_FILE_SIZE" value="100000" />
+			Choose a file to upload: <input name="uploadedfile" type="file" /><br />
+			<input type="submit" value="Upload File" />
+</form>
 </body>
 </html>
 
