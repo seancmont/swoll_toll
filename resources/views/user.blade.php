90aee81e7293c3953bcc0b6ea8e82ab647e4798a
Sean Montgomery <seancmont@gmail.com>
2017-08-25 12:45:40 -0400

    styling changes to workouts and user pages

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -30,7 +30,7 @@
 
 			<form enctype="multipart/form-data" action="uploader.php" method="POST">
 			<input type="hidden" name="MAX_FILE_SIZE" value="100000" />
-			Choose a file to upload: <input name="uploadedfile" type="file" /><br />
+			Choose a file to upload: <input id="uploadPic" name="uploadedfile" type="file" /><br />
 			<input type="button" value="Upload File" />
 </form>
 </body>
